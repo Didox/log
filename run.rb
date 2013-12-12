aa9d68da4b4f9b14c95752f3bbd182742693f217
Didox <didox_59@yahoo.com.br>
2013-12-12 21:08:18 -0200

    bbt

--- run.rb
+++ run.rb
@@ -26,12 +26,12 @@ while(1<2)
 		if contents.include?("agora")
 			File.open(file_repo, 'w') { |file| file.write("ok #{Time.now}") }
 			#bate e cria log
-			File.open("teste/file-#{Time.now.strftime("%d-%m-%Y-%h%m")}", 'w') { |file| file.write("data alterada em #{Time.now}") }
+			File.open("teste/file-#{Time.now.strftime("%d-%m-%Y-%h%m-%s")}", 'w') { |file| file.write("data alterada em #{Time.now}") }
 			commit
 		elsif contents.include?("preview")
 			File.open(file_repo, 'w') { |file| file.write("ok #{Time.now}") }
 			#somente verifica sem bater
-			File.open("teste/preview-#{Time.now.strftime("%d-%m-%Y-%h%m")}", 'w') { |file| file.write("data alterada preview em #{Time.now}") }
+			File.open("teste/preview-#{Time.now.strftime("%d-%m-%Y-%h%m-%s")}", 'w') { |file| file.write("data alterada preview em #{Time.now}") }
 			commit
 		end
 			
