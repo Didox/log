b19833e45cccad1cbc75e74089987a6c5ebb48d1
Didox <didox_59@yahoo.com.br>
2013-12-12 20:34:36 -0200

    bbt

--- run.rb
+++ run.rb
@@ -1,11 +1,28 @@
 file = "repo"
-contents = File.read(file)
+while(1<2)
+	puts "reading file"
 
-if contents.include?("agora")
-	File.open(file, 'w') { |file| file.write("ok") }
-	system "git checkout bbt"
-	system "git add ."
-	system "git commit -m 'commit'"
-	system "git pull"
-	system "git push origin bbt"
+	begin
+
+		system "git reset --hard a37149541b8bf776b8786fc75f7a73461c8534c9"
+		system "git checkout bbt"
+		system "git pull origin bbt"
+
+		sleep(10)
+		contents = File.read(file)
+
+		if contents.include?("agora")
+			File.open(file, 'w') { |file| file.write("ok") }
+			system "git add ."
+			system "git commit -m 'commit'"
+			system "git push origin bbt"
+
+			puts "push success"
+		end
+
+		puts "fininsh"
+
+	rescue Exception => err
+		puts "ERRO: #{err.message}"
+	end
 end
